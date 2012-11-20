c7464a4c6d50616769d57529b3b49a829f53deb5
Peter Donald <peter@realityforge.org>
2012-11-20 15:08:21 +1100

    Remove attribution

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,4 @@
 #
-# Copyright 2011, Peter Donald
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
