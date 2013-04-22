a3202795f5db29dc5625516da8087fc568c2031e
Peter Donald <peter@realityforge.org>
2013-04-22 10:22:27 +1000

    Start to document some of the logstash attributes

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,24 @@ version          '0.0.1'
 depends 'java'
 # Required if any of the ports listened to are below 1024
 recommends 'authbind'
+
+attribute 'logstash/package_url',
+          :display_name => 'logstash jar url',
+          :description => 'URL to monolithic logstash jar.',
+          :type => 'string',
+          :default => 'https://logstash.objects.dreamhost.com/release/logstash-1.1.10-flatjar.jar'
+attribute 'logstash/install_path',
+          :display_name => 'The base install path',
+          :description => 'The location where Logstash jar is installed.',
+          :type => 'string',
+          :default => '/usr/local/logstash'
+attribute 'logstash/config_path',
+          :display_name => 'The config path',
+          :description => 'The location where Logstash config files are stored.',
+          :type => 'string',
+          :default => '/usr/local/logstash/config'
+attribute 'logstash/pattern_path',
+          :display_name => 'The config path',
+          :description => 'The location where Logstash patterns are stored.',
+          :type => 'string',
+          :default => '/usr/local/logstash/patterns'
